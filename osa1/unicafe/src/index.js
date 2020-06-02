6f7b831f527b79ec3ccedd0a491f2d5acd2dcca1
Anna Lehto <anviinamaki@gmail.com>
2020-06-02 14:29:36 +0300

    Osa 1: tehtävä 1.7.

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Stat = (props) => {
+  
+  const sum = () => {
+    return props.good+props.neutral+props.bad
+  }
 
+  if (sum()===0) {
+    return (
+    <div>Palautetta ei ole vielä annettu.</div>
+    )
+  }
+
+  const avg = () => {
+    return props.good+(props.bad*-1) / sum()
+  }
+
+  const posit = () => {
+    if (props.good===0) {
+      return 0
+    }
+    return props.good/props.bad
+  }
+
+  return (
+    <div>
+      <p>Palautteita annettu yhteensä {sum()}</p>
+      <p>Keskiarvo {avg()}</p>
+      <p>Myönteisten palautteiden prosenttiosuus {posit()}</p>
+    </div>
+    )
+} 
 
 const App = (props) => {
   // tallenna napit omaan tilaansa
@@ -12,24 +42,21 @@ const App = (props) => {
   const handleGoodClick = () => {setGood(good+1)}
   const handleNeutralClick = () => {setNeutral(neutral+1)}
   const handleBadClick = () => {setBad(bad+1)}
-
-
+  
   return (
     <div>
       <h2>Anna palautetta</h2>
       <button onClick={handleGoodClick}>Erinomaista</button>
       <button onClick={handleNeutralClick}>Kiitos ruuasta</button>
-      <button onClick={handleBadClick}>Tällä kertaa ei maistunut!</button>
+      <button onClick={handleBadClick}>Tällä kertaa ei maistunut</button>
 
       <h2>Tilasto</h2>
       <p>Erinomaista {good}</p> 
       <p>Kiitos ruuasta {neutral}</p>
       <p>Tällä kertaa ei maistunut {bad}</p>
-
+      <Stat good={good} neutral={neutral} bad={bad}/>
     </div>
   )
-}
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+}
+  ReactDOM.render(<App />, document.getElementById('root'))
